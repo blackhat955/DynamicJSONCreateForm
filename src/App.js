cd44644098caa8daa70095bae1e4ee64b8edd175
vishal-mourya <vishal.maurya2050@gmail.com>
2021-04-10 20:10:34 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -6,77 +6,91 @@ import Select from "react-select";
 import "./App.css";
 
 const value = [
-  { name: "name", value: "Durgesh" },
-  { name: "age", value: "34" },
-  { name: "qualification", value: "B.E" },
-  { name: "College Name", value: "college Name" },
-  { name: "College Name", value: "college Name" },
-  { name: "Address", value: "address" },
-  { name: "Zip Code", value: "select-zip" },
-  { name: "Payment Method", value: "payment" },
-  {
-    options: [
-      { value: "chocolate", label: "Chocolate" },
-      { value: "strawberry", label: "Strawberry" },
-      { value: "vanilla", label: "Vanilla" }
-    ]
-  }
+    { name: "name", value: "Durgesh" },
+    { name: "age", value: "34" },
+    { name: "qualification", value: "B.E" },
+    { name: "College Name", value: "college Name" },
+    { name: "College Name", value: "college Name" },
+    { name: "Address", value: "address" },
+    { name: "Zip Code", value: "select-zip" },
+    { name: "Payment Method", value: "payment" },
+    {
+        options: [
+            { value: "chocolate", label: "Chocolate" },
+            { value: "strawberry", label: "Strawberry" },
+            { value: "vanilla", label: "Vanilla" }
+        ]
+    }
 ];
 class App extends React.Component {
-  state = {
-    selectedOption: null
-  };
-  handleChange = (selectedOption) => {
-    this.setState({ selectedOption });
-    console.log(`Option selected:`, selectedOption);
-  };
-  render() {
-    const { selectedOption } = this.state;
+    state = {
+        selectedOption: null
+    };
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption });
+        console.log(`Option selected:`, selectedOption);
+    };
+    render() {
+        const { selectedOption } = this.state;
 
-    return (
-      <div className="App">
-        <div class="inputSection">
-          {value.map((val, i) =>
-            !val.options ? (
-              <div className="input">
-                <Input name={val.value} placeholder={val.name} />
-              </div>
-            ) : (
-              <div className="select">
-                <Select
-                  value={selectedOption}
-                  onChange={this.handleChange}
-                  options={val.options}
-                  className="selectbox"
-                />
-              </div>
-            )
-          )}
-        </div>
-        <div class="input1">
-          {value.map((val, i) => (
-            <div className="input11">
-              <Radio name="radio1" placeholder={val.name} value={val.value} />
-            </div>
-          ))}
-        </div>
-        <div class="input1">
-          {value.map((val, i) => (
-            <div className="input11">
-              <Radio
-                v="1"
-                name="radio1"
-                placeholder={val.name}
-                value={val.value}
-              />
-            </div>
-          ))}
-        </div>
+        return ( <
+            div className = "App" >
+            <
+            div class = "inputSection" > {
+                value.map((val, i) =>
+                    !val.options ? ( <
+                        div className = "input" >
+                        <
+                        Input name = { val.value }
+                        placeholder = { val.name }
+                        /> < /
+                        div >
+                    ) : ( <
+                        div className = "select" >
+                        <
+                        Select value = { selectedOption }
+                        onChange = { this.handleChange }
+                        options = { val.options }
+                        className = "selectbox" /
+                        >
+                        <
+                        /div>
+                    )
+                )
+            } <
+            /div> <
+            div class = "input1" > {
+                value.map((val, i) => ( <
+                    div className = "input11" >
+                    <
+                    Radio name = "radio1"
+                    placeholder = { val.name }
+                    value = { val.value }
+                    /> < /
+                    div >
+                ))
+            } <
+            /div> <
+            div class = "input1" > {
+                value.map((val, i) => ( <
+                    div className = "input11" >
+                    <
+                    Radio v = "1"
+                    name = "radio1"
+                    placeholder = { val.name }
+                    value = { val.value }
+                    /> < /
+                    div >
+                ))
+            } <
+            /div>
 
-        <Button />
-      </div>
-    );
-  }
+            <
+            Button / >
+            <
+            /div>
+        );
+    }
 }
 
 export default App;
